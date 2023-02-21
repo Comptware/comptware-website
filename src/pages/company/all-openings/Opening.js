1f17eb250343d5abe6f4e314264aff2c7c25dec7
Akinpeju Yusuf <akinpejuyusuf@bani.africa>
2023-02-21 05:16:35 +0100

    build fix

--- src/pages/company/all-openings/Opening.js
+++ src/pages/company/all-openings/Opening.js
@@ -4,40 +4,44 @@ import ArrowRight from "assets/icons/Arrow/arrow-right-blue.svg";
 import BorderFade from "assets/icons/border-fade.svg";
 
 const Opening = ({ details, showBorder }) => {
-  const { location, title, body, href, type } = details;
-
   return (
     <div className="w-full flex flex-col gap-12 lg:gap-14 items-center justify-center ">
       <div className="flex flex-col slg:flex-row justify-between items-start text-left w-full gap-8 md:gap-4 overflow-hidden md:w-[90%] lg:w-[80%]">
         <div className="flex flex-col justify-center items-start text-left w-full h-full space-y-4">
           <div className="flex flex-col md:flex-row justify-start items-start md:items-center text-left gap-4">
-            <h2 className="text-blue text-xl md:text-[23px]">{title}</h2>
+            <h2 className="text-blue text-xl md:text-[23px]">
+              {details?.title}
+            </h2>
 
             <p className="text-black basier-medium bani-title-alt">
-              {location}
+              {details?.location}
             </p>
           </div>
           <p className="text-grey-text font-light leading-loose text-base md:text-[24px] w-[75%] pb-3 md:pb-5">
-            {body}
+            {details?.body}
           </p>
 
           <div className="bg-blue-dull flex justify-center items-center  bani-title-alt rounded-[80px] h-[40px] md:h-[50px] px-6 text-center w-fit text-blue">
-            {type}
+            {details?.type}
           </div>
         </div>
 
         <a
-          href={href}
+          href={details?.href}
           className="flex justify-start items-center space-x-2 underline text-blue"
           target="_blank"
           rel="noreferrer"
         >
-          <span className="basier-medium text-xl md:text-[22px] whitespace-nowrap ">Apply Now</span>
+          <span className="basier-medium text-xl md:text-[22px] whitespace-nowrap ">
+            Apply Now
+          </span>
           <ArrowRight />
         </a>
       </div>
 
-      {showBorder && <BorderFade className="scale-x-[0.25] sm:scale-x-[0.40] md:scale-x-[0.50] lg:scale-x-[0.75]" />}
+      {showBorder && (
+        <BorderFade className="scale-x-[0.25] sm:scale-x-[0.40] md:scale-x-[0.50] lg:scale-x-[0.75]" />
+      )}
     </div>
   );
 };
