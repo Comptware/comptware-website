0293c69b04ca7f28333fef15b7e326c0758c483d
Akinpeju Yusuf <akinpejuyusuf@bani.africa>
2023-03-09 09:44:41 +0100

    New update

--- src/pages/legal/aml-policy/index.js
+++ src/pages/legal/aml-policy/index.js
@@ -7,6 +7,7 @@ import DashboardLayout from "components/layout";
 
 const AmlPolicyList = () => {
   const [pageLoaded, setPageLoaded] = useState(false);
+  
   useEffect(() => {
     setPageLoaded(true);
   }, []);
